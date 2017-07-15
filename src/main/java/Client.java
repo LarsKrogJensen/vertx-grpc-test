190ec7f282f886f26f39da722d27dcca37346575
Lars Krog-Jensen <lars.krogjensen@gmail.com>
2017-07-15 07:51:48 +0200

    stuff

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,19 +1,16 @@
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.stream.IntStream;
 
 public class Client {
     private static final Logger log = LoggerFactory.getLogger(Client.class);
 
     public static void main(String[] args) throws IOException {
         Vertx vertx = Vertx.vertx();
-       
-        IntStream.range(0, 4).forEach(i -> vertx.deployVerticle(new ClientVerticle()));
-
+        vertx.deployVerticle(ClientVerticle.class.getName(), new DeploymentOptions().setInstances(4));
     }
-
 }
 
